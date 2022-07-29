1cd8e35c5681d89ba5c910440d64b39421c1eada
seni12 <frewoinit945@gmail.com>
2022-07-29 15:55:26 -0700

    wip

--- JSFOLDER/cart.js
+++ JSFOLDER/cart.js
@@ -1,5 +1,6 @@
 
 const cartItem = document.querySelector(".cart-item");
+
  
 const makeRequestItemOnCart = async(id)=>{
   let response = await fetch(`http://localhost:8080/item/${id}`);
@@ -14,32 +15,35 @@ const makeRequestItemOnCart = async(id)=>{
 
 const renderItemOnCart = async function(id) {
   
-
-
   let item = await makeRequestItemOnCart(id);
   
      
-    const cartHtml = `<img src="${item.imageURL}" alt="">
+    const cartHtml = `<img src="${item.imageURL}" alt="image">
     <div class="detail">
       <h3 class="name">${item.name}</h3>
-   <p class="description"> ${item.description}</p>
+      <p class="description"> ${item.description}</p>
       <span class="quantity">Quantity:1</span>
    <span class="price">${item.price}</span>
   </div>
-   <div class="cancel" id="cancelBtn" onclick="removeFromCart()"><i class="fa-solid fa-trash"></i></div>`
-   cartItem.innerHTML+=cartHtml;
-   let cancel = document.getElementsByClassName("cancel")
+   <div class="cancel" id="cancelBtn"><i class="fa-solid fa-trash"></i></div>`
 
-    function removeFromCart(){
-    for(let i=0;i<cancel.length;i++){
-       let button = cancel[i];
-       button.addEventListener('click',()=>{
-        button.parentElement.remove()
-       });
-    }
-   }
-  removeFromCart();
-
+     
+         cartItem.innerHTML+=cartHtml;
+     
+   //cartItem.innerHTML+=cartHtml;
+    
+   let cancel = document.querySelector("#cancelBtn")
+       console.log(cancel);
+        let parent = cancel.parentNode
+        console.log(parent);
+    function removeFromCart(id){
+        cancel.addEventListener('click',()=>{
+          cancel.childNodes[item.id].remove()
+        })
+        
+       
+   } 
+   removeFromCart();
 }
 
 const cartIcon = document.querySelector(".fa-cart-shopping");
